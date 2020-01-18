ba4eed9730522283f17b94bd24f1c1a7fe08aeec
spettigrew <sara.pettigrew5@gmail.com>
2020-01-17 20:00:08 -0700

    Server file initated and routers imported

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
+
+const authRouter = require("./auth/auth-router")
+const userRouter = require("./users/user-router")
+
+const server = express()
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+
+require("dotenv").config()
+
+server.use("/users", userRouter)
+server.use("/auth", authRouter)
+
+server.get("/", (req, res, next) => {
+    res.status(201).json({ message: "Welcome to this API."})
+})
+
+server.use((err, req, res, next) => {
+    console.log("Error:", err)
+    res.status(500).json({ message: "Incorrect, check your work." })
+})
+
+module.export = server;

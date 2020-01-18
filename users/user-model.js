019a46db3551ea091bf8732a62db274d8483e8a3
spettigrew <sara.pettigrew5@gmail.com>
2020-01-18 16:06:50 -0700

    Started setting up model file

---
+++ users/user-model.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcryptjs")
+const db = require("../data/db-config")
+
+const jwt = require("jsonwebtoken")
+
+function findBy(filter) {
+    return db("users")
+    .where(filter)
+    .select("id", "username", "password", "department")
+}
+
+async function add(user) {
+    user.password = await bcrypt.hash(user.password, 12)
+
+    const [id] = await db("users")
+    .insert(user)
+
+    return findById(id)
+}
+
+function findById(id) {
+    return db("users")
+    .where({ id })
+    .first("id", "username")
+}
+
+module.exports = {
+    find,
+    findBy,
+    findById,
+    add,
+}

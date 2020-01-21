4a4b9ec62e33cafa44d8c2f3d1ab76d523534069
spettigrew <sara.pettigrew5@gmail.com>
2020-01-21 15:29:11 -0700

    Secret process setup in config file

--- data/db-config.js
+++ data/db-config.js
@@ -3,5 +3,11 @@ const knex = require('knex')
 const knexfile = require("../knexfile")
 
 const environment = process.env.NODE_ENV || "development"
+const secret = process.env.JWT_SECRET || "Unicorns are not real, sorry."
 
-module.exports = knex(knexfile[environment])
+module.exports = knex(knexfile[environment]),
+secret
+
+    // {
+    //     jwt.Secret: process.env.JWT_SECRET || "Unicorns are not real, sorry."
+    // }

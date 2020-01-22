cdbbd7a2c0cde38f6c3acdc8c91c53898cd144c3
spettigrew <sara.pettigrew5@gmail.com>
2020-01-21 19:31:13 -0700

    Notes added

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken")
+const secrets = require("../config/secrets")
 
 
 module.exports = (req, res, next) => {
@@ -21,3 +22,20 @@ module.exports = (req, res, next) => {
  }
 }
 // jwt is a synchronous function; does not need async/await.
+
+// Notes from Jason's code.
+// module.exports = () => {
+//     return (req, res, next) => {
+//         try {
+//             const token = req.headers.authorization
+//             const decoded = jwt.verify(token, secrets.jwt)
+
+//             req.userId = decoded.subject
+//             next()
+//         } catch (err) {
+//             return res.status(401).json({
+//                 message: "Invalid credentials",
+//             })
+//         }
+//     }
+// }

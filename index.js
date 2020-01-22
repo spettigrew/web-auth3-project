1ca1afdda138fbdaee2c06ad0f0e24ac18b78b98
spettigrew <sara.pettigrew5@gmail.com>
2020-01-21 19:27:30 -0700

    Notes added

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 
 const server = require('./server');
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
+server.listen(port, () => {
+    console.log(`Listening on port ${port}...`)
 });
+
+// Notes from Jason's code.
+
+// server.listen(port, () => {
+//     console.log(`\n** Running on http://localhost:${port} **\n`)
+// })

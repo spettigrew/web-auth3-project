b45d529ab90528deeece08af30bb832e9acaabad
spettigrew <sara.pettigrew5@gmail.com>
2020-01-23 19:13:56 -0700

    File renamed

--- config/secrets.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    
-    jwtSecret: process.env.JWT_SECRET || "Unicorns are not real, sorry.",
-    options: { 
-        expiresIn: process.env.JWT_EXPIRATION || "2h" 
-    } 
-
-}
